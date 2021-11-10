e67ca60b68b784348b44fd7b883bf329027dc616
Andaru <andarudanurdara@yahoo.com>
2021-11-10 19:55:18 +0700

    Up Polimorfisme yang Java

---
+++ Polimorfisme/java/Kuda.java
@@ -0,0 +1,6 @@
+public class Kuda extends Mamalia{
+    public void info(){
+        System.out.println("Kuda merupakan mamalia yang berasal dari genus Equus.");
+        System.out.println("Kuda sudah mulai dimanfaatkan untuk keperluan manusia sejak 2000 SM.\n");
+    }
+}
